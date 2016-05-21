eeac327ae4f5c56b695cd9a5b01b336bfc0e9131
yamit <yamit.cardenas@gmail.com>
2016-05-21 10:31:21 -0500

    agregados prototipos

--- src/code/webVariables.js
+++ src/code/webVariables.js
@@ -1,37 +1,32 @@
 var CalculadorDePrecios = function(){
-  var descuentos=[0.95,0.93,0.90,0.85];
-  var rangos =[ 1000,5000,10000,20000];
-  var impuestos = {
+  this.descuentos=[0.95,0.93,0.90,0.85];
+  this.rangos =[ 1000,5000,10000,20000];
+  this.impuestos = {
         "Colombia":1.16,
         "Perú":1.13,
         "Mexico":1.2,
         "Argentina":1.06,
         "Panama":1.1
   };
-  //PUBLICO
-  this.calcular = function(cantidad,precioUnitario,pais){
-   
-      var precioTotal= calcularDescuento(cantidad,precioUnitario);   
-      return calcularImpuesto(precioTotal,pais);
+}
+
+CalculadorDePrecios.prototype.calcularImpuesto=function(precioTotal,pais){
+      return Math.round((precioTotal * this.impuestos[pais])*100)/100;
   };
-  //PRIVADO
-  var calcularImpuesto=function(precioTotal,pais){
-      return Math.round((precioTotal * impuestos[pais])*100)/100;
-  };
-  var calcularDescuento = function(cantidad,precioUnitario){
+
+CalculadorDePrecios.prototype.calcularDescuento = function(cantidad,precioUnitario){
     var total = cantidad*precioUnitario;
-      for (var i= descuentos.length - 1; i >= 0; i--){
-          if (total>rangos[i]){
-            total = total * descuentos[i];
+      for (var i= this.descuentos.length - 1; i >= 0; i--){
+          if (total>this.rangos[i]){
+            total = total * this.descuentos[i];
             break;
           }
       }
       return total;
   };
-  
-}
+CalculadorDePrecios.prototype.calcular = function(cantidad,precioUnitario,pais){
+   
+      var precioTotal= this.calcularDescuento(cantidad,precioUnitario);   
+      return this.calcularImpuesto(precioTotal,pais);
+};
 
-var cal= [];
-for(var i = 0;i< 1000000;i++){
-  cal.push(new CalculadorDePrecios());
-}
