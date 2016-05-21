71c6a17d6aa4b017683d887be7cfcfddd971205e
yamit <yamit.cardenas@gmail.com>
2016-05-21 15:37:18 -0500

    Batalla naval con eventos y html

--- deploy/js/code.js
+++ deploy/js/code.js
@@ -1,4 +1,29 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+var juego;
+
+window.onload = function(){
+  
+
+  juego= new BatallaNaval();
+  juego.prepararJuego([[2,3],[3,1]],[[2,1],[2,2]]);
+  juego.jugador1.tableroEnemigo.dibujarTablero();
+
+  var tds = document.getElementsByTagName("td");
+  for(var i = 0; i<tds.length; i++){
+    tds[i].onclick = shoot;
+  }
+}
+
+var shoot = function(event){
+
+  var id = event.target.id;
+  var coordenadas = id.split("-");
+  juego.jugador1.disparar(coordenadas[1],coordenadas[2]);
+  
+
+}
+
+
 var Tablero = function(coordenadas){
 
   this.tablero = [
@@ -15,15 +40,32 @@ var Tablero = function(coordenadas){
 
 Tablero.prototype.dibujarTablero = function(){
 
-    for(var fila = 0 ; fila <this.tablero.length ; fila++ ){
-      var filaUno = " ";
+    var tabla = document.getElementById("jugador1");
+    
+    //borrar datos de la tabla
+    var filas = tabla.getElementsByTagName("tr");
+    var length = filas.length
+    for(var i = length-1; i>=0; i--){
+      tabla.deleteRow(i);  
+    }
 
-      for(var col = 0 ; col<this.tablero[fila].length ; col++){
-        filaUno+=(this.tablero[fila][col] + " ");
-
+    for(var i = 0; i < this.tablero.length; i++){
+      var fila = document.createElement("tr");
+      for(var j = 0; j < this.tablero[i].length; j++){
+        var colItem = document.createElement("td");
+        if(juego.jugador1.tableroEnemigo.tablero[i][j]==="x"){
+          colItem.className = "disparoFallido";  
+        }else if(juego.jugador1.tableroEnemigo.tablero[i][j]==="p"){
+          colItem.className = "disparo"; 
+        }else{
+          colItem.className = "mar"; 
+        }
+  
+        colItem.id = "j1-"+i+"-"+j;
+        colItem.onclick = shoot;
+        fila.appendChild(colItem);
       }
-        console.log(filaUno);
-      
+      tabla.appendChild(fila);  
     }
 }
 
@@ -39,13 +81,13 @@ Jugador.prototype.disparar = function(fila,col){
     this.tableroEnemigo.tablero[fila][col]="x";
   }
   else if(this.tableroEnemigo.tablero[fila][col]==="b"){
-    this.tableroEnemigo.tablero[fila][col]="P";
+    this.tableroEnemigo.tablero[fila][col]="p";
   }
   this.tableroEnemigo.dibujarTablero();
 }
 
 
-var BatallaNaval= function (){
+var BatallaNaval = function (){
    this.jugador1 ;
    this.jugador2 ;
 
@@ -56,16 +98,15 @@ BatallaNaval.prototype.prepararJuego=function(coordenadasJ1,coordenadasJ2){
 
   this.jugador1.tableroEnemigo=this.jugador2.tableroPropio;
   this.jugador2.tableroEnemigo=this.jugador1.tableroPropio;
+
+  
 };
 
 
 
-var juego= new BatallaNaval();
 
-juego.prepararJuego([[2,3],[3,1]],[[2,1],[2,2]]);
 
-juego.jugador1.disparar(3,2);
-juego.jugador2.disparar(2,3);
+
 
 
 
--- src/code/webVariables.js
+++ src/code/webVariables.js
@@ -1,3 +1,28 @@
+var juego;
+
+window.onload = function(){
+  
+
+  juego= new BatallaNaval();
+  juego.prepararJuego([[2,3],[3,1]],[[2,1],[2,2]]);
+  juego.jugador1.tableroEnemigo.dibujarTablero();
+
+  var tds = document.getElementsByTagName("td");
+  for(var i = 0; i<tds.length; i++){
+    tds[i].onclick = shoot;
+  }
+}
+
+var shoot = function(event){
+
+  var id = event.target.id;
+  var coordenadas = id.split("-");
+  juego.jugador1.disparar(coordenadas[1],coordenadas[2]);
+  
+
+}
+
+
 var Tablero = function(coordenadas){
 
   this.tablero = [
@@ -14,15 +39,32 @@ var Tablero = function(coordenadas){
 
 Tablero.prototype.dibujarTablero = function(){
 
-    for(var fila = 0 ; fila <this.tablero.length ; fila++ ){
-      var filaUno = " ";
+    var tabla = document.getElementById("jugador1");
+    
+    //borrar datos de la tabla
+    var filas = tabla.getElementsByTagName("tr");
+    var length = filas.length
+    for(var i = length-1; i>=0; i--){
+      tabla.deleteRow(i);  
+    }
 
-      for(var col = 0 ; col<this.tablero[fila].length ; col++){
-        filaUno+=(this.tablero[fila][col] + " ");
-
+    for(var i = 0; i < this.tablero.length; i++){
+      var fila = document.createElement("tr");
+      for(var j = 0; j < this.tablero[i].length; j++){
+        var colItem = document.createElement("td");
+        if(juego.jugador1.tableroEnemigo.tablero[i][j]==="x"){
+          colItem.className = "disparoFallido";  
+        }else if(juego.jugador1.tableroEnemigo.tablero[i][j]==="p"){
+          colItem.className = "disparo"; 
+        }else{
+          colItem.className = "mar"; 
+        }
+  
+        colItem.id = "j1-"+i+"-"+j;
+        colItem.onclick = shoot;
+        fila.appendChild(colItem);
       }
-        console.log(filaUno);
-      
+      tabla.appendChild(fila);  
     }
 }
 
@@ -38,13 +80,13 @@ Jugador.prototype.disparar = function(fila,col){
     this.tableroEnemigo.tablero[fila][col]="x";
   }
   else if(this.tableroEnemigo.tablero[fila][col]==="b"){
-    this.tableroEnemigo.tablero[fila][col]="P";
+    this.tableroEnemigo.tablero[fila][col]="p";
   }
   this.tableroEnemigo.dibujarTablero();
 }
 
 
-var BatallaNaval= function (){
+var BatallaNaval = function (){
    this.jugador1 ;
    this.jugador2 ;
 
@@ -55,16 +97,15 @@ BatallaNaval.prototype.prepararJuego=function(coordenadasJ1,coordenadasJ2){
 
   this.jugador1.tableroEnemigo=this.jugador2.tableroPropio;
   this.jugador2.tableroEnemigo=this.jugador1.tableroPropio;
+
+  
 };
 
 
 
-var juego= new BatallaNaval();
 
-juego.prepararJuego([[2,3],[3,1]],[[2,1],[2,2]]);
 
-juego.jugador1.disparar(3,2);
-juego.jugador2.disparar(2,3);
+
 
 
 
